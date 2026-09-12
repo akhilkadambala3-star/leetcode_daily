4cb052588aa1664ad003201f362bbd69be0bc549
akhilkadambala3-star <akhilkadambala3@gmail.com>
2026-09-12 11:46:39 +0000

    add digits problem

---
+++ easy/58_add_digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        while(num>=10){
+            int sum=0;
+        
+            while(num !=0){
+                int digits=num % 10;
+                num=num/10;
+                sum=sum+digits;
+
+            }
+            num=sum;
+        }
+        return num;    
+        
+    }
