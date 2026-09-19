796b04366df042a943b24e06451d0839ab50840c
akhilkadambala3-star <akhilkadambala3@gmail.com>
2026-09-19 18:29:37 +0000

    lc que

---
+++ easy/125_validpalindrome.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isAlphanum(char ch){
+        if(ch>='0'&& ch<='9' ||tolower(ch)>='a'&& tolower(ch)<='z'){
+            return true;
+        }
+        return false;
+    }
+    bool isPalindrome(string s) {
+        int start=0,end=s.size()-1;
+        while(start<end){
+            if(!isAlphanum(s[start])){
+                start++;
+                continue;
+            }
+            if(!isAlphanum(s[end])){
+                end--;
+                continue;
+            }
+            if(tolower(s[start])!=tolower(s[end])){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+       
+
+        
+    }
+};
